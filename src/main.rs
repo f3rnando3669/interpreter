b6c581d7fa5f5ed0541794defc71917ce55ae006
f3rnando3669 <112440567+f3rnando3669@users.noreply.github.com>
2024-02-21 21:04:13 -0800

    Update main.rs
    updated code

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,33 @@
-// #[derive(Copy, Clone)]
-pub enum Expression {
-    Add(Vec<Expression>),
-    Multiply(Vec<Expression>),
-    Number(i32)
-}
-
-pub fn evaluate_addition(expression: &Expression) -> i32 {
-    if let Expression::Add(expressions) = expression {
-        let iter = expressions.iter();
-        iter.fold(0, |total, next| total + evaluate(next))
-    } else {
-        panic!("addition not provided")
+enum Primitive {
+ Add, 
+ Multiply, 
+ Number(i32), 
+} 
+fn eval_prim(p: &Primitive) -> i32 {
+     match p {
+     Primitive::Add => 0, 
+     Primitive::Multiply => 0, 
+     Primitive::Number(val) => *val
+      }
+     }
+fn evaluate(v: Vec<Primitive>) -> i32 {
+    match v[0] { 
+        Primitive::Add => {eval_prim(&v[1]) + eval_prim(&v[2]) }, 
+        Primitive::Multiply => { eval_prim(&v[1]) * eval_prim(&v[2]) }, 
+        _=> 0
     }
 }
-
-pub fn evaluate_multiply(expression: &Expression) -> i32 {
-    if let Expression::Multiply(expressions) = expression {
-        let iter = expressions.iter();
-        iter.fold(1, |total, next| total * evaluate(next))
-    } else {
-        panic!("addition not provided")
-    }
-}
-
-pub fn evaluate(expression: &Expression) -> i32 {
-    match expression {
-        Expression::Add(_) => evaluate_addition(expression),
-        Expression::Multiply(_) => evaluate_multiply(expression),
-        Expression::Number(val) => *val
-    }
-}
-
 fn main() {
-    let addition = Expression::Add(vec![Expression::Number(2), Expression::Number(2)]);
-    println!("2 + 2 is {}", evaluate(&addition));
-}
+    let mut expression = Vec::<Primitive>::new(); 
+    expression.push(Primitive::Add); 
+    expression.push(Primitive::Number(3)); 
+    expression.push(Primitive::Number(4)); 
+    let sum = evaluate(expression); 
 
-// Arrange
-// Act
-// Assert
-
-// Given
-// When
-// Then
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2+2, 4);
-    }
-
-    #[test]
-    fn test_addition() {
-        // Arrange
-        let three = crate::Expression::Number(3);
-        let four = crate::Expression::Number(4);
-        let addition = crate::Expression::Add(vec![three, four]);
-        // Act
-        let sum = crate::evaluate_addition(&addition);
-        // Assert
-        assert_eq!(sum, 7);
-    }
-
-    #[test]
-    fn test_multiply() {
-        // Arrange
-        let three = crate::Expression::Number(3);
-        let four = crate::Expression::Number(4);
-        let multiplication = crate::Expression::Multiply(vec![three, four]);
-        // Act
-        let product = crate::evaluate_multiply(&multiplication);
-        // Assert
-        assert_eq!(product, 12);
-    }
+    let mut expression2 = Vec::<Primitive>::new();
+    expression2.push(Primitive::Multiply);
+    expression2.push(Primitive::Number(2));
+    expression2.push(Primitive::Number(5));
+    let sum2 = evaluate(expression2);
+    println!("{sum} and {sum2}");
 }
